45c580545fd655ce974ede6c3bb025c69ab7fb49
Muhaiminur Rahman <muhaiminurabir@gmail.com>
2018-05-22 11:25:06 +0600

    Dexter Permission Manager Added

--- app/src/main/java/com/muhaiminurabir/permissionmanager/MainActivity.java
+++ app/src/main/java/com/muhaiminurabir/permissionmanager/MainActivity.java
@@ -7,6 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.karan.churi.PermissionManager.PermissionManager;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,13 +23,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        permissionManager=new PermissionManager() {};
+        //permission manager one
+        /*permissionManager=new PermissionManager() {};
         permissionManager.checkAndRequestPermissions(this);
         //runtime permission check
         if (checkPermission()){
 
-        }
+        }*/
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.ACCESS_NETWORK_STATE,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.VIBRATE,
+                        Manifest.permission.SYSTEM_ALERT_WINDOW
+                ).withListener(new MultiplePermissionsListener() {
+            @Override public void onPermissionsChecked(MultiplePermissionsReport report) {/* ... */}
+            @Override public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {/* ... */}
+        }).check();
     }
+    //permission manager one
+    /*
     private boolean checkPermission(){
         int result = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION);
         if (result == PackageManager.PERMISSION_GRANTED){
@@ -38,5 +63,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
         permissionManager.checkResult(requestCode,permissions, grantResults);
-    }
+    }*/
 }
